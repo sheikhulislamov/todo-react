7b85ad727a4f03cefde71c7a8a1e82f2bd0211a3
Arsen <arsen.sheikhulislamov@protonmail.com>
2022-06-27 22:18:28 +0300

    Adding Nanoid library to make unique keys

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import React, { useState } from 'react';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
+import { nanoid } from 'nanoid';
 
 function App(props) {
 
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name) {
-    alert(name);
+    const newTask = {
+      id: "todo-" + nanoid(), 
+      name: name,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks.map(task => (
+  const taskList = tasks.map(task => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed} 
       key={task.id}
     />
-  ));
+  )
+  );
 
   return (
     <div className="todoapp stack-large">
